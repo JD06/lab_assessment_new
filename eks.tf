bf1966fddbc4cce92d4be45922fd28d9e5f617c9
Jaydeep Shome <jaydeepshome06@gmail.com>
2026-04-07 17:04:18 +0530

    Terminating the lab

--- eks.tf
+++ eks.tf
@@ -1,34 +1,34 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.0"
 
-  cluster_name    = "my-python-app-cluster"
-  cluster_version = "1.29"
-  vpc_id          = aws_vpc.main.id
-  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  enable_irsa     = true
-  cluster_endpoint_public_access           = true
+#   cluster_name    = "my-python-app-cluster"
+#   cluster_version = "1.29"
+#   vpc_id          = aws_vpc.main.id
+#   subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+#   enable_irsa     = true
+#   cluster_endpoint_public_access           = true
 
 
-  # Managed Node Groups configuration
-  eks_managed_node_groups = {
-    default = {
+#   # Managed Node Groups configuration
+#   eks_managed_node_groups = {
+#     default = {
 
-      # m6a.large is great, but t3.medium is the smallest recommended for EKS.
-      instance_types = ["t3.medium"]
+#       # m6a.large is great, but t3.medium is the smallest recommended for EKS.
+#       instance_types = ["t3.medium"]
 
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
+#       min_size     = 2
+#       max_size     = 3
+#       desired_size = 2
 
-      subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+#       subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 
-      capacity_type = "ON_DEMAND"
-    }
-  }
+#       capacity_type = "ON_DEMAND"
+#     }
+#   }
 
-  tags = {
-    Environment = "production"
-    Application = "python-ip-app"
-  }
-}
+#   tags = {
+#     Environment = "production"
+#     Application = "python-ip-app"
+#   }
+# }
