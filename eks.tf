5a3358e747e59b1124ddb3f5fae6360479138fac
Jaydeep Shome <jaydeepshome06@gmail.com>
2026-04-07 15:15:48 +0530

    Reprovision eks clluster

--- eks.tf
+++ eks.tf
@@ -1,35 +1,34 @@
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 20.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-#   cluster_name                             = "my-python-app-cluster"
-#   cluster_version                          = "1.29"
-#   vpc_id                                   = aws_vpc.main.id
-#   subnet_ids                               = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-#   authentication_mode                      = "API_AND_CONFIG_MAP"
-#   enable_cluster_creator_admin_permissions = true
-#   cluster_endpoint_public_access           = true
-  
+  cluster_name    = "my-python-app-cluster"
+  cluster_version = "1.29"
+  vpc_id          = aws_vpc.main.id
+  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  enable_irsa     = true
+  cluster_endpoint_public_access           = true
 
-#   # Managed Node Groups configuration
-#   eks_managed_node_groups = {
-#     general = {
 
-#       # m6a.large is great, but t3.medium is the smallest recommended for EKS.
-#       instance_types = ["t3.medium"]
+  # Managed Node Groups configuration
+  eks_managed_node_groups = {
+    default = {
 
-#       min_size     = 2
-#       max_size     = 3
-#       desired_size = 2
+      # m6a.large is great, but t3.medium is the smallest recommended for EKS.
+      instance_types = ["t3.medium"]
 
-#       subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
 
-#       capacity_type = "ON_DEMAND"
-#     }
-#   }
+      subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 
-#   tags = {
-#     Environment = "production"
-#     Application = "python-ip-app"
-#   }
-# }
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "python-ip-app"
+  }
+}
